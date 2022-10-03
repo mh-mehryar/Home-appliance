eab83eb296e2d23d753ed2552882a7e1c4b64b71
Mohammad Hossein <mh.mehryar@gmail.com>
2022-10-03 23:03:43 +0330

    Product Category Management emplemented

--- Home_Appliance_Project/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
+++ Home_Appliance_Project/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
@@ -14,7 +14,7 @@ namespace ShopManagement.Domain.ProductCategoryAgg
         public string Picture { get;  private set; }
         public string PictureAlt { get; private set; }
         public string PictureTitle { get;  private set; }
-        //public string MyProperty { get; private set; }
+  
         public string Keywords { get; private set; }
         public string MetaDescription { get; private set; }
         public string Slug { get; private set; }
